a06e697d4810ffafd9bd1554a4933d5ae1d60fdc
steve_D <scd346@nyu.edu>
2017-04-21 15:02:03 +0200

    Updated carousel for page2.

--- js/init.js
+++ js/init.js
@@ -6,3 +6,9 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+	$("#theId").on('click', function(){
+	    window.location = "page 2.html";    
+	});
+});
